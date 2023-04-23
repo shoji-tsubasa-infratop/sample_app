eaf83417a8d8b3504f08254cd7ca43191daea5ac
shoji-tsubasa-infratop <131357847+shoji-tsubasa-infratop@users.noreply.github.com>
2023-04-23 07:44:14 +0000

    [Finish]4-11

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'lists/new'
+  get '/top' => 'homes#top'
   resources :lists
   # post 'lists' => 'lists#create' 
   # get 'lists' => 'lists#index'
@@ -7,6 +8,5 @@ Rails.application.routes.draw do
   # get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
   # patch 'lists/:id' => 'lists#update', as: 'update_list'
   # delete 'lists/:id' => 'lists#destroy', as: 'destroy_list'
-  # get '/top' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
